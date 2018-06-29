e63e3cef86524952f870ae7c6b62e65ceadf985f
ankithkumar <ankith.b01@gmail.com>
2018-06-30 00:54:53 +0530

    added multiple images to circle through at 3 second interval

--- main.js
+++ main.js
@@ -5,12 +5,20 @@ let greetMessage = {
    night : 'Good Night'
 }
 
+let images = [
+    'https://irisholidays.com/keralatourism/wp-content/uploads/2013/01/athirappilly-hill-station.jpg',
+    'https://tmi2-tourmyindiapvtlt.netdna-ssl.com/blog/wp-content/uploads/2013/10/ooty-hill-station1.jpg',
+    'http://hdwallpapersrocks.com/wp-content/uploads/2013/10/Hill-station-nature.jpg',
+    'https://media.cntraveller.in/wp-content/uploads/2017/10/Hills-lead-866x487.jpg',
+    'http://www.tripmytravel.com/wp-content/uploads/2017/07/Kodaikanal-Hill-station-images1.jpg',
+]
+
 let greetPerson = function (greetIdOfDom) {
     let currentDate = getDateObject();
     let time = calculateTimeFromDate(currentDate);
 
     if (time.meridian === 'AM') {
-        if (time.hours >=12 && time.hours <=4) {
+        if (time.hours >=12 && time.hours <= 16) {
             greetIdOfDom.innerHTML = greetMessage.night;
         } else if (time.hours >= 4 && time.hours <= 12) {
             greetIdOfDom.innerHTML = greetMessage.morning;
@@ -27,7 +35,7 @@ let greetPerson = function (greetIdOfDom) {
 }
 
 let checkForMeridian = function (hours) {
-    return (hours >= 0 && hours <12) ? 'AM' : (hours >= 12 && hours <=23) ? 'PM' : '';
+    return (hours >= 0 && hours < 12) ? 'AM' : (hours >= 12 && hours <=23) ? 'PM' : '';
 }
 
 let convertHoursToFitMeridian = function (hours) {
@@ -65,11 +73,20 @@ focusOn = function (domObject) {
     domObject.focus();
 }
 
+changeImageInIntervals = function(bodyDomObject) {
+    window.setInterval(() => {
+        let index = Math.floor(Math.random() * 1000) % 4;
+        bodyDomObject.style.backgroundImage = `url(${images[index]})`;
+    }, 3000);
+}
+
 initialise = function() {
     let timeIdOfDomObject = document.getElementById('time-of-day');
     let greetIdOfDomObject = document.getElementById('greet-message');
     let forTheDayIdOfDomObject = document.getElementById('for-the-day');
+    let bodyIdOfDomObject = document.getElementById('body-id')
     setTimeInIntervals(timeIdOfDomObject);
     greetPerson(greetIdOfDomObject);
     focusOn(forTheDayIdOfDomObject);
+    changeImageInIntervals(bodyIdOfDomObject);
 }
